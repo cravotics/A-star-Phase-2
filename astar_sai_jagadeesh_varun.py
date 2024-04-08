4c48600c9f9c19bb23cdf6ceb9f20b842168fc54
Crave Robotics <jagadeesh.offcl2k@gmail.com>
2024-04-07 22:13:15 -0400

    comments added

--- astar_sai_jagadeesh_varun.py
+++ astar_sai_jagadeesh_varun.py
@@ -18,8 +18,8 @@ Graph_map = np.ones((height, width, 3), dtype=np.uint8)*255
 def start_node(width, height, canvas):
     while True:
         try:
-            Xs = int(input("Enter the x-coordinate of the start node(Xs): "))
-            start_y = int(input("Enter the y-coordinate of the start node(Ys): "))
+            Xs = int(input("Enter the x-coordinate of the start node(Xs) in cm: "))
+            start_y = int(input("Enter the y-coordinate of the start node(Ys) in cm: "))
             Ys = height - start_y
             start_theta = int(input("Enter the angle of the start_node: "))
             
@@ -38,8 +38,8 @@ def start_node(width, height, canvas):
 def goal_node(width, height, canvas):
     while True:
         try:
-            Xg = int(input("Enter the x-coordinate of the goal node(Xg): "))
-            goal_y = int(input("Enter the y-coordinate of the goal node(Yg): "))
+            Xg = int(input("Enter the x-coordinate of the goal node(Xg)in cm: "))
+            goal_y = int(input("Enter the y-coordinate of the goal node(Yg) in cm: "))
             Yg = height - goal_y            
             if Xg < 0 or Xg >= width or Yg < 0 or Yg >= height:
                 print("The x and y coordinates of the goal node is out of range.Try again!!!")
@@ -55,7 +55,7 @@ def step_size_function():
     while True:
         try:
             RPM1 = int(input("Enter the RPM 1 : "))
-            RPM2 = int(input("Enter the RPM 2  "))
+            RPM2 = int(input("Enter the RPM 2 :  "))
             if 1 <= RPM1 <= 200 and 1 <= RPM2 <= 200:
                 return RPM1, RPM2
             else:
@@ -128,36 +128,33 @@ for x in range(width):
         if ((x-420)**2 + (y_transform -120)**2 <= (60)**2):
             Graph_map[y, x] = [0,0,0]  
 
-# plot the graph map
-
-
+#--------------------------------------checking the valid nodes--------------------------------------#
 def is_valid(x, y, Graph_map):
     if x >= 0 and x < width and y >= 0 and y < height:
         # return np.all(Graph_map[int(y), int(x)] == [255, 255, 255])
-        print(Graph_map[y, x])
         return all(Graph_map[y, x] == [255, 255, 255])
     else:
         return False
+#--------------------------------------Possible Node Function--------------------------------------#
     
 def possible_node(current_node, RPM1, RPM2, Graph_map, L = 28.7, R = 3.3):  
     dt = 0.1 
+    # RPM to radian per second conversion
     ul_vel = RPM1*R*(2*np.pi/60)
     ur_vel = RPM2*R*(2*np.pi/60)
-    Curr_Node_X = current_node[0] #Grab Current Node X
-    Curr_Node_Y = current_node[1] #Grad Current Node Y
-    Curr_Node_Theta = np.deg2rad(current_node[2]) #Grab Current Node Theta, convert to radians.
-    distance_cost = 0.0 #Init Cost
+    currn_node_x = current_node[0] 
+    curren_node_y = current_node[1] 
+    curren_node_theta = np.deg2rad(current_node[2]) 
+    distance_cost = 0.0 
     actions = [(0, ul_vel), (ul_vel,0) ,(ul_vel,ul_vel), (0,ur_vel), (ur_vel,0), (ur_vel,ur_vel), (ul_vel,ur_vel), (ur_vel,ul_vel)] #All Possible Actions
     for action in actions:
         ul, ur = action
         t = 0
         velocities = []
-
-        next_x = Curr_Node_X #Set New Node Start Point X
-        next_y = Curr_Node_Y #Set New Node Start Point Y
-        next_theta = Curr_Node_Theta #Set New Node Start Point Theta
-
-        ##----------------Euler Integration to Generate Curvature----------------##
+        next_x = currn_node_x 
+        next_y = curren_node_y 
+        next_theta = curren_node_theta 
+#--------------------------------------Calculating the next x, y and theta--------------------------------------#
         while t < 1:
             t += dt
             linear_vel = 0.5*(ul+ur)
@@ -165,14 +162,14 @@ def possible_node(current_node, RPM1, RPM2, Graph_map, L = 28.7, R = 3.3):
             del_x = 0.5*(ul+ur)*np.cos(next_theta)*dt
             del_y = 0.5*(ul+ur)*np.sin(next_theta)*dt
             del_theta = (1/L)*(ur-ul)*dt
-            velocities.append((float(linear_vel), float(angular_vel)))
+            velocities.append(((linear_vel), (angular_vel))) # Storing the linear and angular velocities    
             next_x += del_x
             next_y += del_y
             next_theta += del_theta
             
 
-        next_x = int(round(next_x))  # Convert to integer
-        next_y = int(round(next_y))  # Convert to integer
+        next_x = int(round(next_x)) # Rounding off the next x and y coordinates  
+        next_y = int(round(next_y))  # Rounding off the next x and y coordinates
         next_theta = np.rad2deg(next_theta)
 
         if (next_theta < 0):
@@ -185,6 +182,7 @@ def possible_node(current_node, RPM1, RPM2, Graph_map, L = 28.7, R = 3.3):
         if 0 <= next_x < width and 0 <= next_y < height:
             if is_valid(next_x, next_y, Graph_map) and not visited_check((next_x, next_y, next_theta)):
                 distance_cost = np.sqrt((current_node[0] - next_x)**2 + (current_node[1] - next_y)**2)
+                cv2.line(Graph_map, (current_node[0], current_node[1]), (next_x, next_y), (37, 78, 66), 1)
                 print("Distance Cost: ", distance_cost, "Next X: ", next_x, "Next Y: ", next_y, "Next Theta: ", next_theta)
                 yield (distance_cost, (next_x, next_y, next_theta), velocities)
 
@@ -233,7 +231,6 @@ def A_star(start_node, goal_node, RPM1, RPM2):
                 cost_total = cost_to_come + heuristic(new_node, goal_node) 
                 open_list.put((cost_total, new_node))
                 marking_visited(new_node)
-                map_visualization[new_node[1], new_node[0]] = [0, 0, 255]
                 if step_count % 100 == 0:
                     output.write(map_visualization)
                 step_count += 1
@@ -274,15 +271,17 @@ def A_star_Backtracting(parent, start_node, end_node, map_visualization, step_co
     parent_and_velocities.reverse()
     all_vels = []
     for i in range(len(parent_and_velocities) - 1):
-        start_point, init_vel = ((parent_and_velocities[i][0][0]), parent_and_velocities[i][0][1]), parent_and_velocities[i][1]
-        end_point = (parent_and_velocities[i + 1][0][0]), (parent_and_velocities[i + 1][0][1])
+        start_point, init_vel = ((parent_and_velocities[i][0][0]), parent_and_velocities[i][0][1]), parent_and_velocities[i][1] # Getting the start point and initial velocities.
+        end_point = (parent_and_velocities[i + 1][0][0]), (parent_and_velocities[i + 1][0][1]) # Getting the end point.
         x_start, y_start = start_point
         print("Start Point: ", x_start, y_start)
         x_end, y_end = end_point
-        print("Start Point: ", x_end, y_end)
+        print("End Point: ", x_end, y_end)
+        cv2.arrowedLine(map_visualization, start_point, end_point, (255, 255, 0), 1, tipLength=0.2) 
         for lin_vel, ang_vel in init_vel:
+            # Storing the linear and angular velocities in meters and radians.
             all_vels.append((lin_vel/100, ang_vel))
-            cv2.arrowedLine(map_visualization, (int(x_start), int(y_start)), (int(x_end), int(y_end)), (0, 255, 0), 1)
+           
             if step_count % 5 == 0:
                 output.write(map_visualization)
             step_count += 1 
@@ -307,9 +306,6 @@ else:
 
 end_time = time.time()    # end of runtime
 print(f'Runtime : {((end_time-start_time)/60):.2f} Minutes')
-print("The video file has been saved as a_star_varun_lakshmanan_sai_jagadeesh_muralikrishnan.mp4")
 
-output.release()
-cv2.imshow("Graph_map", Graph_map)
-cv2.waitKey(0)
+
 #--------------------------------------------------End of the Program-------------------------------------------------#
